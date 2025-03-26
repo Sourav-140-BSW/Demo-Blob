1e40aa5a57aeb93f6660352b073417049f9fc04b
Subham Sahu <subham.sahu@brand-scapes.com>
2025-03-26 15:34:45 +0530

    updated settings file

--- backend/files/models.py
+++ backend/files/models.py
@@ -15,7 +15,7 @@ from django.db import models
 class UploadedFile(models.Model):
     name = models.CharField(max_length=255)
     azure_url = models.URLField(max_length=500)
-    azure_filename = models.CharField(max_length=255)  # Stores the unique filename in Azure
+    azure_filename = models.CharField(max_length=255, default="temp_default")  # Stores the unique filename in Azure
     file_size = models.IntegerField()
     uploaded_at = models.DateTimeField(auto_now_add=True)
 
